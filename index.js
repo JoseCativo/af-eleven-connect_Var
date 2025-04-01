4c3c01995d906e12ba3f7f6cf0f75043f5f92dcd
JoseCativo <125588041+JoseCativo@users.noreply.github.com>
2025-04-01 11:06:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGODB_URI || "mongodb://localhost:27017/clientsDB"
+      process.env.MONGODB_URI
     );
     console.log("MongoDB connected successfully");
   } catch (err) {
